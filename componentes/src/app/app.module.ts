5827581b775c46888f92789040a8540d53deda4c
tello <tello@gmail.com>
2017-09-13 10:34:22 +0200

    componentes

---
+++ componentes/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { CommonsModule } from './commons/commons.module';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
